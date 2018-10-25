0c2192a64583a40a4deea4d911aa2c3b13b3e86d
filipefreitassilva <34353641+filipefreitassilva@users.noreply.github.com>
2018-10-25 12:52:01 -0300

    Atualização Firebase
    Atualização Firebase

--- app/src/main/java/meuveterinario/filipe/com/br/meuveterinrio/CadastrarActivity.java
+++ app/src/main/java/meuveterinario/filipe/com/br/meuveterinrio/CadastrarActivity.java
@@ -1,6 +1,7 @@
 package meuveterinario.filipe.com.br.meuveterinrio;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,10 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class CadastrarActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText text_emailcadastro, text_senhacadastro, text_repetirsenhacadastro;
     private Button button_cadastrarusuario, button_cancelar;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,9 @@ public class CadastrarActivity extends AppCompatActivity implements View.OnClick
 
         button_cadastrarusuario.setOnClickListener(this);
         button_cancelar.setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance();
+
     }
 
     @Override
@@ -44,6 +54,41 @@ public class CadastrarActivity extends AppCompatActivity implements View.OnClick
     }
 
     private void cadastrar(){
-        Toast.makeText(this, "Cadastrar Clicado",Toast.LENGTH_LONG).show();
+
+        String email = text_emailcadastro.getText().toString().trim();
+        String senha = text_senhacadastro.getText().toString().trim();
+        String confirmasenha = text_repetirsenhacadastro.getText().toString().trim();
+
+        if (email.isEmpty() || senha.isEmpty() || confirmasenha.isEmpty()){
+            Toast.makeText(getBaseContext(), "Preencha os campos necessários!", Toast.LENGTH_LONG).show();
+        } else {
+
+
+            if (senha.contentEquals(confirmasenha)) {
+
+                criarUsuario(email, senha);
+
+            } else {
+                Toast.makeText(getBaseContext(), "Senhas diferentes!", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
+
+    private void criarUsuario(String email, String senha){
+
+        auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(getBaseContext(), "Cadastro efetuado com sucesso!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "Erro ao cadastrar!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/meuveterinario/filipe/com/br/meuveterinrio/LoginEmailActivity.java
+++ app/src/main/java/meuveterinario/filipe/com/br/meuveterinrio/LoginEmailActivity.java
@@ -3,11 +3,40 @@ package meuveterinario.filipe.com.br.meuveterinrio;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginEmailActivity extends AppCompatActivity {
+public class LoginEmailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText text_emaillogin, text_senhalogin;
+    private Button button_oklogin, button_recuperarsenha;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginemail);
+
+        text_emaillogin = (EditText)findViewById(R.id.text_emaillogin);
+        text_senhalogin = (EditText)findViewById(R.id.text_senhalogin);
+
+        button_oklogin = (Button)findViewById(R.id.button_oklogin);
+        button_recuperarsenha = (Button)findViewById(R.id.button_recuperarlogin);
+
+        button_oklogin.setOnClickListener(this);
+        button_recuperarsenha.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_oklogin:
+
+                break;
+
+            case R.id.button_recuperarlogin:
+
+                break;
+        }
     }
 }
